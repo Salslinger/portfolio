ea984252c7ec68420d25924416354b970c465dbb
Kenny Nguyen <knguyen5574@gmail.com>
2025-05-13 17:13:47 -0500

    testing react

---
+++ src/assets/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../../types/todo";
+
+export const dummyData: Todo[] = [
+   {
+       id: 1,
+       title: "Lock In",
+       completed: false, 
+    },
+   {
+       id: 2,
+       title: "Lock In Again",
+       completed: false, 
+    },
+   {
+       id: 3,
+       title: "Lock in x3",
+       completed: false, 
+    },
+]
